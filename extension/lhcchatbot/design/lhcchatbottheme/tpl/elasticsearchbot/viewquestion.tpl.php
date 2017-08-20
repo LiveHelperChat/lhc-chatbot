b37e8d18977d6cc1683f83823537b3d47ba136f2
Remdex <remdex@gmail.com>
2017-08-20 18:42:21 +0200

    More info

--- extension/lhcchatbot/design/lhcchatbottheme/tpl/elasticsearchbot/viewquestion.tpl.php
+++ extension/lhcchatbot/design/lhcchatbottheme/tpl/elasticsearchbot/viewquestion.tpl.php
@@ -5,8 +5,8 @@
     <input type="text" class="form-control" name="question" value="<?php echo htmlspecialchars($question->question)?>" />
 </div>
 
-<ul>
+<ul class="list-unstyled">
     <?php foreach ($items as $item) : ?>
-        <li><?php echo htmlspecialchars($item->answer)?> | matched - <?php echo $item->match_count?>, [<?php echo $item->chat_id?>]</li>
+        <li><a title="<?php echo $question->chat_id?>" href="#" onclick="lhc.previewChat(<?php echo $question->chat_id?>)"><i class="material-icons">info_outline</i></a> [<?php echo $item->match_count?>] - <?php echo htmlspecialchars($item->answer)?></li>
     <?php endforeach; ?>
 </ul>
