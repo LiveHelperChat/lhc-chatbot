340ee1904902871a40a74e1113dbcf975c5faf19
Remdex <remdex@gmail.com>
2017-09-18 18:27:13 +0200

    Allow index just custom departments

--- extension/lhcchatbot/modules/lhcron/index_chats.php
+++ extension/lhcchatbot/modules/lhcron/index_chats.php
@@ -1,19 +1,25 @@
 <?php
 
-// /usr/bin/php cron.php -s site_admin -e lhcchatbot -c cron/index_chats
+// /usr/bin/php cron.php -s site_admin -e lhcchatbot -c cron/index_chats [-p <dep_id>,<dep_id>]
+
+$filter = array();
+
+if ($cronjobPathOption->value != '') {
+    $filter['filterin']['dep_id'] = explode(',',$cronjobPathOption->value);
+}
 
 echo "Indexing chats\n";
 
 $pageLimit = 100;
 
-$parts = ceil(erLhcoreClassModelChat::getCount()/$pageLimit);
+$parts = ceil(erLhcoreClassModelChat::getCount($filter)/$pageLimit);
 
 for ($i = 0; $i < $parts; $i++) {
 
     echo "Saving chat page - ",($i + 1),"\n";
 
-    $items = erLhcoreClassModelChat::getList(array('offset' => $i*$pageLimit, 'limit' => $pageLimit, 'sort' => 'id ASC'));
-        
+    $items = erLhcoreClassModelChat::getList(array_merge_recursive(array('offset' => $i*$pageLimit, 'limit' => $pageLimit, 'sort' => 'id ASC'), $filter));
+
     erLhcoreClassElasticSearchChatboxIndex::indexChats(array('chats' => $items));
 }
 
