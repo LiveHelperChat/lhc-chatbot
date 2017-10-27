b99621600c9a975acfbd83310aa43c7775c2a37a
Remdex <remdex@gmail.com>
2017-10-27 14:00:04 +0200

    Fix test page

--- extension/lhcchatbot/design/lhcchatbottheme/tpl/lhcchatbot/test.tpl.php
+++ extension/lhcchatbot/design/lhcchatbottheme/tpl/lhcchatbot/test.tpl.php
@@ -7,6 +7,18 @@
         <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('xmppservice/form','Question');?></label>
         <input type="text" class="form-control" name="question"  value="<?php isset($_GET['question']) ? print htmlspecialchars($_GET['question']) : ''?>" />
     </div>
+
+    <div class="form-group">
+        <label><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('xmppservice/form','Context');?></label>
+        <?php echo erLhcoreClassRenderHelper::renderCombobox( array (
+            'input_name'     => 'context_id',
+            'display_name'   => 'name',
+            'css_class'      => 'form-control',
+            'selected_id'    =>  isset($_GET['context_id']) ? (int)$_GET['context_id'] : 0,
+            'list_function'  => 'erLhcoreClassModelLHCChatBotContext::getList',
+            'list_function_params'  => array_merge(array('limit' => '1000000')),
+        )); ?>
+    </div>
     
     <?php if (isset($answer)) : ?>
         <div class="panel panel-default panel-info">
