6c857755b759a038653e5dfcc51dfde93b581d76
Remigijus Kiminas <remdex@gmail.com>
2019-03-19 05:50:28 -0400

    Prefill message area option

--- extension/lhcchatbot/design/lhcchatbottheme/js/lhcchatbot.js
+++ extension/lhcchatbot/design/lhcchatbottheme/js/lhcchatbot.js
@@ -19,7 +19,7 @@ var lhcChatBot = {
 
                     $.each(item, function (i, itemSuggest) {
                         if ($('#' + chat_id + '-' + itemSuggest.aid).length == 0) {
-                            var li = jQuery('<li class="lhc-new-suggest list-inline-item pl-1 pb-1" ><button id="' + chat_id + '-' + itemSuggest.aid + '" onclick="lhcChatBot.sendSuggest(' + chat_id + ',$(this))" type="button" class="btn btn-sm btn-light text-left">' + jQuery('<p/>').text(itemSuggest.a).html() + '</button> <button type="button" class="btn btn-xs btn-danger" title="' + jQuery('<p/>').text(itemSuggest.in_response).html() + '" onclick="return lhcChatBot.sendNegative(' + chat_id + ',$(this))"><i class="material-icons mr-0 fs11">delete</i></button></li>').attr('title', jQuery('<p/>').text(itemSuggest.q).html());
+                            var li = jQuery('<li class="lhc-new-suggest list-inline-item pl-1 pb-1" ><button type="button" class="btn btn-sm btn-light" title="Prefill message field" data-title="' + jQuery('<p/>').text(itemSuggest.a).html() + '" onclick="return lhcChatBot.prefill(' + chat_id + ',$(this))"><i class="material-icons mr-0 fs11">edit</i></button> <button id="' + chat_id + '-' + itemSuggest.aid + '" onclick="lhcChatBot.sendSuggest(' + chat_id + ',$(this))" type="button" class="btn btn-sm btn-light text-left">' + jQuery('<p/>').text(itemSuggest.a).html() + '</button> <button type="button" class="btn btn-xs btn-danger" title="' + jQuery('<p/>').text(itemSuggest.in_response).html() + '" onclick="return lhcChatBot.sendNegative(' + chat_id + ',$(this))"><i class="material-icons mr-0 fs11">delete</i></button></li>').attr('title', jQuery('<p/>').text(itemSuggest.q).html());
                             containerSuggest.prepend(li);
                         }
                     });
@@ -34,6 +34,11 @@ var lhcChatBot = {
                 })
             });
         }
+    },
+
+    prefill : function(chat_id,inst)
+    {
+        $("#CSChatMessage-" + chat_id).val(inst.attr('data-title'));
     },
 
     sendNegative: function (chat_id, inst) {
--- extension/lhcchatbot/design/lhcchatbottheme/tpl/lhcchatbot/invalid.tpl.php
+++ extension/lhcchatbot/design/lhcchatbottheme/tpl/lhcchatbot/invalid.tpl.php
@@ -27,8 +27,9 @@
                     <td><?php echo htmlspecialchars($item->counter)?></td>
                     <td nowrap>
                         <div class="btn-group" role="group" aria-label="..." style="width:60px;">
-                            <a class="btn btn-warning btn-xs csfr-required" href="<?php echo erLhcoreClassDesign::baseurl('lhcchatbot/deletereport')?>/<?php echo $item->id?>" title="Only report will be deleted"><i class="material-icons mr-0">&#xE254;</i>Delete report</a>
-                            <a class="btn btn-danger btn-xs csfr-required" onclick="return confirm('<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('kernel/messages','Are you sure?');?>')" title="Report and related bot training will be deleted" href="<?php echo erLhcoreClassDesign::baseurl('lhcchatbot/deleteall')?>/<?php echo $item->id?>" ><i class="material-icons mr-0">&#xE872;</i>Confirm report</a>
+                            <?php /*<a class="btn btn-info btn-xs csfr-required" href="<?php echo erLhcoreClassDesign::baseurl('lhcchatbot/deletereport')?>/<?php echo $item->id?>" title="Edit training"><i class="material-icons mr-0">&#xE254;</i>Edit</a>*/ ?>
+                            <a class="btn btn-warning btn-xs csfr-required" href="<?php echo erLhcoreClassDesign::baseurl('lhcchatbot/deletereport')?>/<?php echo $item->id?>" title="Only report will be deleted"><i class="material-icons mr-0">&#xE254;</i>Ignore</a>
+                            <a class="btn btn-danger btn-xs csfr-required" onclick="return confirm('<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('kernel/messages','Are you sure?');?>')" title="Report and related bot training will be deleted" href="<?php echo erLhcoreClassDesign::baseurl('lhcchatbot/deleteall')?>/<?php echo $item->id?>" ><i class="material-icons mr-0">&#xE872;</i>Delete</a>
                         </div>
                     </td>
                 </tr>
