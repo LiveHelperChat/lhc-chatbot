944b211021e4b74d7b4a241a6984f340383bee66
Remigijus Kiminas <remdex@gmail.com>
2020-09-09 05:54:27 -0400

    Avoid clearing cache on API fail

--- extension/lhcchatbot/modules/lhlhcchatbot/suggest.php
+++ extension/lhcchatbot/modules/lhlhcchatbot/suggest.php
@@ -5,9 +5,15 @@ session_write_close();
 erLhcoreClassChat::validateFilterIn($Params['user_parameters_unordered']['id']);
 
 if (!empty($Params['user_parameters_unordered']['id'])) {
-    $suggestions = erLhcoreClassExtensionLHCChatBotValidator::suggestByIds($Params['user_parameters_unordered']['id'],($Params['user_parameters_unordered']['chat'] == 1 ? true : false));
-    $chatsUnsupported = erLhcoreClassExtensionLHCChatBotValidator::getUnsupportedChats($Params['user_parameters_unordered']['id'],($Params['user_parameters_unordered']['chat'] == 1 ? true : false));
-    echo json_encode(array('un' => $chatsUnsupported, 'sg' => $suggestions));
+    try {
+        $suggestions = erLhcoreClassExtensionLHCChatBotValidator::suggestByIds($Params['user_parameters_unordered']['id'],($Params['user_parameters_unordered']['chat'] == 1 ? true : false));
+        $chatsUnsupported = erLhcoreClassExtensionLHCChatBotValidator::getUnsupportedChats($Params['user_parameters_unordered']['id'],($Params['user_parameters_unordered']['chat'] == 1 ? true : false));
+        echo json_encode(array('un' => $chatsUnsupported, 'sg' => $suggestions));
+    } catch (Exception $e) {
+        if (erConfigClassLhConfig::getInstance()->getSetting('site', 'debug_output')) {
+            throw $e;
+        }
+    }
 }
 
 exit;
