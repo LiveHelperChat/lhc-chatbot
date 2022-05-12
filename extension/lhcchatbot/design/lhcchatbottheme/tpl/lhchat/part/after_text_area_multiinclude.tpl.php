3db18c3ce4da29e74659b5d0e5a5f11c889e4f02
Remigijus Kiminas <remdex@gmail.com>
2022-05-12 07:40:50 -0400

    Option turn off auto suggester

--- extension/lhcchatbot/design/lhcchatbottheme/tpl/lhchat/part/after_text_area_multiinclude.tpl.php
+++ extension/lhcchatbot/design/lhcchatbottheme/tpl/lhchat/part/after_text_area_multiinclude.tpl.php
@@ -1,22 +1,25 @@
-<?php 
-
-$lhcChatBotLast5Messages = array();
-
-foreach (array_reverse($messages) as $msg) {
-    if ($msg['user_id'] == 0) {
-        $lhcChatBotLast5Messages[] = (int)$msg['id'];
-    }
-    if (count($lhcChatBotLast5Messages) == 5) {
-        break;
-    }
-}
-
-if (erLhcoreClassUser::instance()->hasAccessTo('lhlhcchatbot','use') == false) {
+<?php
+if (erLhcoreClassModule::getExtensionInstance('erLhcoreClassExtensionLhcchatbot')->settings['enabled'] == true) {
     echo "<script>lhcChatBot.disabled = true;</script>";
-}
+} else {
+    $lhcChatBotLast5Messages = array();
 
-if (!empty($lhcChatBotLast5Messages)) {
-    echo "<script>lhcChatBot.syncadmin({'chatbotids':". json_encode($lhcChatBotLast5Messages) ."})</script>";
+    foreach (array_reverse($messages) as $msg) {
+        if ($msg['user_id'] == 0) {
+            $lhcChatBotLast5Messages[] = (int)$msg['id'];
+        }
+        if (count($lhcChatBotLast5Messages) == 5) {
+            break;
+        }
+    }
+
+    if (erLhcoreClassUser::instance()->hasAccessTo('lhlhcchatbot','use') == false) {
+        echo "<script>lhcChatBot.disabled = true;</script>";
+    }
+
+    if (!empty($lhcChatBotLast5Messages)) {
+        echo "<script>lhcChatBot.syncadmin({'chatbotids':". json_encode($lhcChatBotLast5Messages) ."})</script>";
+    }
 }
 
 ?>
