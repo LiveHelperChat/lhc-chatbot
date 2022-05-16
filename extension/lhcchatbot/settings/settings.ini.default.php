9e4fa6368d77ad3108abcd74345965570a86f7ce
Remigijus Kiminas <remdex@gmail.com>
2022-05-16 03:09:43 -0400

    Update default

--- extension/lhcchatbot/settings/settings.ini.default.php
+++ extension/lhcchatbot/settings/settings.ini.default.php
@@ -1,7 +1,7 @@
 <?php 
 
 return array(
-    'enable' => true,
+    'enabled' => true,
     'id' => 1,                          // Instance ID, just leave 1 in standalone mode, and set 0 in automated hosting environment
     'try_times' => 2,                   // How many times send request for the same message. As we are using random response and we want more than one suggestion so this simulates it.
     'ahosting' => false,                // Are we in automated hosting environment
